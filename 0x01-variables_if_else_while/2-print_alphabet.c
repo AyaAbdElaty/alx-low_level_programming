290c9293d0f5febeb7f95aa217e6a237f414a677
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 10:58:03 -0700

    alp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,12 @@
  * main -Entry point
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ++ch)
 	{
-		putchar(ch);
+		putchar (ch+1);
 	}
 	return(0);
 }
