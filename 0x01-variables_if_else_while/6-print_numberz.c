dd8b69d44f58c5435926b9c4f033101bc9e019bc
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 13:18:25 -0700

    print num

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
  * main -Entry point
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num ++)
+	for (num = 0; num < 10; num++)
 	{
-		putchar(num);
+		putchar(num + '0');
 	}
 	putchar('\n');
         return (0);
