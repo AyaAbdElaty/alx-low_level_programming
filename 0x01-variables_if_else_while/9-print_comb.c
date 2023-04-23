d81e995d3cd59585d5cede4d6c13e70b9dcb9c59
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 13:52:03 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
