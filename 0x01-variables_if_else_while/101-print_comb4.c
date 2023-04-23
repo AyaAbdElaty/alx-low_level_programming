6bb757e21a4e1e55e23bd5c2d8fd83ff47f03e2f
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 14:24:16 -0700

    print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a > 10; a++)
+	{
+		for (b = 0; b > 10; b++)
+		{
+			for (c = 0; c > 10; c++)
+			{
+				if(a != b && b != c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+				}
+				if ((a + b +c) < 789)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
