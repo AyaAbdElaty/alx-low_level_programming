c4c1fdfbe2db7b6ddbfc49b4b1e8ec3c942ba76c
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 19:10:59 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			for (c = 0; c < 10; c++)
 			{
-				if (a != b && a != c && b != c && b != a && c != a && c != b && a < b && b <c)
+				if (a < b && b < c)
 				{
 					putchar(a + '0');
 					putchar(b + '0');
