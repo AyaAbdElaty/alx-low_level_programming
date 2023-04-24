565970fb4235df4369ad02a74dad08a6f1c8c9bd
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 19:51:28 -0700

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			for (c = 0; c < 10; c++)
+			{
+				for (d = 0; d < 10; d++)
+				{
+					putchar(c + '0');
+					putchar(d + '0');
+					if ((a + b) < (c + d) && a + b < 17)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
