901d37ef82c28e89b297adc61ec6dc806d794928
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-23 19:30:48 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,7 @@ int main(void)
 {
 	int a;
 	int b;
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
@@ -15,7 +16,7 @@ int main(void)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
-				if ( a + b < 17)
+				if (a + b < 17)
 				{
 					putchar(',');
 					putchar(' ');
