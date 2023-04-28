ed964f0bf1c8c9642f6045de981ce95675b56e46
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-28 10:25:34 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - is the name of the function
+ *
+ * @void: is the parametar of the function
+ * Description: the function is to check case sensitve of a character
+ * Return: is used to return zero or one based on case sensitive
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 || a != 52)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
