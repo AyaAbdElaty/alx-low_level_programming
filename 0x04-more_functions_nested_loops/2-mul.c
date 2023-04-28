927a43bd6db3df4d34fdfe998e62e4dc819d8073
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-28 08:54:12 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - is the name of the function
+ *
+ * @a: is the parametar of the function
+ * @b: is the parameter of the function
+ * Description: the function is to multiply two digits
+ * Return: is used to return the result of multiplicaion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
