d7272912e10dd66e6efb174246e9fc0c762050bb
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-28 09:30:47 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - is the name of the function
+ *
+ * @void: is the parametar of the function
+ * Description: the function is to check case sensitve of a character
+ * Return: is used to return zero or one based on case sensitive
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+	{
+		_putchar(a);
+		-putchar('\n' + 48);
+	}
+}
