04f84b15edcea92c65ec7a4cc89190a163f873ae
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-04-28 13:18:56 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,16 @@ void more_numbers(void)
 {
 	int a;
 	int i;
-	int rem;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			rem = a;
 			if (a > 9)
 			{
-				rem = a % 10;
-				_putchar(1 + '0');
+				_putchar('1');
 			}
-			_putchar(rem + '0');
+			_putchar(a % 10 + '0');
 		}
 		_putchar('/n');
 	}
