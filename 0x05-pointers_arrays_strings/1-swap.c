d067e2360ee94d7f12362ed338c1edfdb920aa42
aya <ayaabdelaty33@gmail.com>
2023-05-03 01:19:47 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "PandAHeaders.h"
+
+/**
+ * swap_int - is the name of the function
+ * @a: is the parameter of the function
+ * @b: is the parameter of the function
+ * Describition: function to swap values
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
