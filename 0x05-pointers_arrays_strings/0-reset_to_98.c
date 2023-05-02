78ad52a854acb554cbd38a2e083af69fa2687065
aya <ayaabdelaty33@gmail.com>
2023-05-03 01:09:37 +0300

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,9 @@
 /**
  * reset_to_98 - is the name of the function
  *
- * @*n: is the parametar of the function
+ * @*n: is the pointer of the function
  * Description: the function is to take a pointer to an int as a parameter
+ * @n: is the parameter
  */
 
 void reset_to_98(int *n)
