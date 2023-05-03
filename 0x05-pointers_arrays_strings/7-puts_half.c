90a895c15cdf128f0cc31c18bf03fe9ed4578d45
aya <ayaabdelaty33@gmail.com>
2023-05-04 02:56:06 +0300

    divide

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half- is the function name
+ *
+ * @str: is the argument of the function
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len;
+	len = _strlen(str);
+	int e , o;
+
+	while (str[i])
+	{
+		if (len % 2 == 0)
+		{
+			e = len / 2;
+			_putchar(str[e]);
+		}
+		else
+		{
+			o = (len -1) /2;
+			_putchar(str[0]);
+		}
+		i++;
+	}
+}
