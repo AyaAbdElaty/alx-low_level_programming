797a1fb40ddbc2e35a3ddde8ef40ac5ce82eb27f
aya <ayaabdelaty33@gmail.com>
2023-05-03 11:04:18 +0300

    putchar

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,18 @@
  /**
   * _strlen - is the name of the function
   *  @s: is the parametar of the function
-  * Description: the function is know the length of the character
+  * Description : the function is know the length of the character
   */
 
 int _strlen(char *s)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; *(s + n) != '\0'; n++);
-		
+	while (*(s + n) != '\0')
+	{
+		n++;
+		s++;
+	}
+	_putchar(n);
 	return (n);
 }
