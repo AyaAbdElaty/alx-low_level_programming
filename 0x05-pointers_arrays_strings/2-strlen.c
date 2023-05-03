c1aaf5a35a4768c86086e891bbd7667d7eda1dbe
aya <ayaabdelaty33@gmail.com>
2023-05-03 04:43:53 +0300

    len

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
  /** _isupper - is the name of the function
-  *
   * @s: is the parametar of the function
   * Description: the function is know the length of the character
   */
@@ -11,5 +10,6 @@ int _strlen(char *s)
 	int n;
 	
 	for (n=0; *(s + n) != '\0'; n++);
+	
 	return (n);
 }
