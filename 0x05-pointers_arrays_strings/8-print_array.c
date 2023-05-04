044ce05bafdb10c7707e9234ac792b48c83ed3ee
aya <ayaabdelaty33@gmail.com>
2023-05-04 04:37:20 +0300

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - is the function name
+ *
+ * @a: is the function argument
+ * @n: is the function argument
+ * return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	a[n];
+	for (i = 0; a[n] != '\0'; i++)
+	{
+		printf("a[%d] = %i\n", n, a[n]);
+	}
+
+}
+	
