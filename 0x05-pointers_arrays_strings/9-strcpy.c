f0a21383408415f67be8b34479147e060671abc5
aya <ayaabdelaty33@gmail.com>
2023-05-04 05:33:22 +0300

    cp

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,10 @@
 /**
  * _strcpy- is the name of the function
  *
+ * description: copy string
  * @dest: is the argument of the function
  * @src: is the argument of the function
- * return: return
+ * Return: pointer to dust
  */
 char *_strcpy(char *dest, char *src)
 {
