3499599df53c02d229f3d2fbfb78077f2ace025e
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-05-05 15:44:59 -0700

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,23 @@
 /**
  * _strcmp - is the name of the function
  *
- * @s1: is function variable
- * @s2: is function variable
- * return: return strcmp
+ * @s1: is the parameter of the function
+ * @s2: is the variable of the function
+ * Return: return strcmp
  */
 int _strcmp(char *s1, char *s2)
 {
+	int re = 0;
 
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			re = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (re);
+}
