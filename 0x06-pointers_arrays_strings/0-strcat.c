f4b45247c1315e5c44b3d105c11c3750ef4bed63
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-05-05 11:49:02 -0700

    con

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (j; src[j]; j++)
+	for (j = 0; src[j]; j++)
 	{
 		dest[i++] = src[j];
 	}
