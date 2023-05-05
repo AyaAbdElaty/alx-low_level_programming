b2b664f5f0714caa000ff994831b0400b1d7b463
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-05-05 15:56:12 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reverse_array- is the name of the function
+ *
+ * @a: is the argument of the function
+ * @n: is the argument of the funtion
+ * Description: the function that inverse numbers
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+		_putchar(a[i]);
+}
