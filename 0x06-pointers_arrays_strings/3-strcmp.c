7bb0a0860161a9cd346ac3003f8e9202321e731e
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-05-05 15:42:48 -0700

    str

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcmp - is the name of the function
+ *
+ * @s1: is function variable
+ * @s2: is function variable
+ * return: return strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+
