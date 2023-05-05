d6bc6cad45c0075e271e38c34e12312111499606
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-05-05 13:22:25 -0700

    conc

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - is the name of the functtion
+ * @dest: is the parameter of the function
+ * @src: is the parameter of the function
+ * @n: is the parameter of the function
+ * Return: return the dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	for (j = 0; j <= n; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
