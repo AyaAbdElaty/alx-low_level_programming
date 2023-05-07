1d1c488c08fb2b35f1fe1b1b9a801cd780b85c14
aya <ayaabdelaty33@gmail.com>
2023-05-07 04:30:29 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - is the name of the function
+ * @n: is the parameter
+ * Return: return values depand on result
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if ((n = 0))
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
