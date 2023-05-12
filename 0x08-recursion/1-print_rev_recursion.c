707350191277cefeb953223b4477ba0f0c8f8b40
aya <ayaabdelaty33@gmail.com>
2023-05-12 12:56:21 +0300

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
+	int n, i;
+	n = 0;
 
-	while (s[i])
+	while (s[n])
 	{
-		i++;
+		n--;
 	}
+	i = n--;
 	_putchar(s[i++]);
 	_print_rev_recursion((s - 1));
 }
