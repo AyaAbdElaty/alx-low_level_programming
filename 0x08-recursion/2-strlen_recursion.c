748bdc561c559622ade7089b120fc93679360f05
aya <ayaabdelaty33@gmail.com>
2023-05-12 16:30:30 +0300

    len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,10 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
-		return (0);
-	_strlen_recursion((s + 1));
-	return (i++);
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
 }
