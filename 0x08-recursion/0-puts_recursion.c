0b1dbb1864bf7455b15581705621f8898a1b99de
aya <ayaabdelaty33@gmail.com>
2023-05-12 11:06:12 +0300

    putchar

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion: is the name of the function
+ * s: is parameter of the function
+ * 
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+}
