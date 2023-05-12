b4a7bbfeb2b8d041fee3a79abff21f9d19418913
aya <ayaabdelaty33@gmail.com>
2023-05-12 11:56:26 +0300

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * _puts_recursion: is the name of the function
- * s: is parameter of the function
- * 
+ * @s: is parameter of the function
  */
 void _puts_recursion(char *s)
 {
 	int i = 0;
 
 	if (s[i] == '\0')
-		return;
+		_putchar('\n');
 	_putchar(s[i]);
-	_putchar('\n');
 	_puts_recursion((s + 1));
 }
