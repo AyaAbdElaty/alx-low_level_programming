ccb3441d190e99532381628ef3d637b316bcdb47
aya <ayaabdelaty33@gmail.com>
2023-05-16 05:56:58 +0300

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,11 @@
  * main - is the main function
  * @argc: is the parameter for count
  * @argv: is the parameter for array
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
+	(void)argc;
 	printf("%s \n", argv[argc]);
-	return(0);
+	return (0);
 }
