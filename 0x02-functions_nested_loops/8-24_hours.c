f02e406098f3abe7652ec19923d5d7d45055d235
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-06-04 06:49:16 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,39 +9,18 @@ void jack_bauer(void)
 
 	for (d = 0; d <= 2; d++)
 	{
-		if (d == 2)
+		for (c = 0; c <= 9; c++)
 		{
-			for (c = 0; c <= 3; c++)
+			for (b = 0; b <= 5; b++)
 			{
-				for (b = 0; b <= 5; b++)
+				for (a = 0; a <= 9; a++)
 				{
-					for (a = 0; a <= 9; a++)
-					{
-						_putchar(d + 48);
-						_putchar(c + 48);
-						_putchar(':');
-						_putchar(b + 48);
-						_putchar(a + 48);
-						_putchar('\n');
-					}
-				}
-			}
-		}
-		else
-		{
-			for (c = 0; c <= 9; c++)
-			{
-				for (b = 0; b <= 5; b++)
-				{
-					for (a = 0; a <= 9; a++)
-					{
-						_putchar(d + 48);
-						_putchar(c + 48);
-						_putchar(':');
-						_putchar(b + 48);
-						_putchar(a + 48);
-						_putchar('\n');
-					}
+					_putchar(d + 48);
+					_putchar(c + 48);
+					_putchar(':');
+					_putchar(b + 48);
+					_putchar(a + 48);
+					_putchar('\n');
 				}
 			}
 		}
