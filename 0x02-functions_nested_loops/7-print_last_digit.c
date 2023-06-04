1d87c955655cef3ba80572f6bdf84cdd339f13bc
AyaAbdElaty <ayaabdelaty33@gmail.com>
2023-06-04 06:09:46 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,12 @@ int print_last_digit(int a)
 	if (a >= 0)
 	{
 		a = a % 10;
-		_putchar(a + '0');
-		return (a);
 	}
 	else
 	{
 		a *= -1;
 		a = a % 10;
-		_putchar(a + '0');
-		return (a);
 	}
+	_putchar(a + '0');
+	return (a);
 }
