a35f13f80d146644d1ac4678c1fbd27e27d6b0d5
Aya <ayaabdelaty33@gmail.com>
2023-06-07 18:11:42 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,24 @@
 #include "main.h"
+int the_prime(int n, int i);
 /**
  * is_prime_number - the defination of the function
  * @n: is the parameter of the function
  */
 int is_prime_number(int n)
 {
-	int i = 0;
-
-	if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11)
+	if (n <= 1)
+		return (0);
+	return (the_prime(n, n - 1));
+}
+/**
+ * the_prime - the defination of the function
+ * @n: is the parameter of the function
+ */
+int the_prime(int n, int i)
+{
+	if (i == 1)
 		return (1);
-	if (n == ((6 * i) - 1))
-	{
-		return 1;
-		is_prime_number((6 * (i + 1)) - 1);
-	}
-	if (n <= 0)
-		return (-1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (the_prime(n, i - 1));
 }
