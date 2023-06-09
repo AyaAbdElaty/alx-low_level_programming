e0f6f00af2e9b940540928cf48d65297a67fca3d
Aya <ayaabdelaty33@gmail.com>
2023-06-09 15:04:53 -0700

    whats

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /*
- * main -Entry point
+ * main - prints program name
  * @argc: is the parameter for count
  * @argv: is the parameter for array
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
-	(void) argc;
+	(void) argc
 	printf("%s\n", argv[0]);
 	return (0);
 }
