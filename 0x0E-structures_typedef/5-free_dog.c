80b8d38dad1b53e72e6901cb986b1e368ae0c7cf
aya <ayaabdelaty33@gmail.com>
2023-06-21 15:30:47 +0300

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free space
+ * @d: is the dog name
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		if((*d).name)
+			free((*d).name);
+		if((*d).owner)
+			free((*d).owner);
+		free(d);
+	}
+}
