e54fba864722bc074bcffd480206c97402be2f01
aya <ayaabdelaty33@gmail.com>
2023-08-04 05:56:19 -0700

    static

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs- is the function name
+ * @a: is the parameter of the function
+ * Return: the value of int
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a *= -1;
+		return (a);
+	}
+	return (a);
+}
